b156787b7bfbc1a9e4db2892a6430877f4cf014f
jester <jaimebuddy98@gmail.com>
2022-04-07 16:03:53 -0500

    resume Content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
         <nav id="navbar" className="nav-menu navbar navbar-expand-lg">
             <ul className="">
                 <li><a href="#about" className="nav-link scrollto"><span>About</span></a></li>
-                <li><a href="#skills" className="nav-link scrollto"><span>Skills</span></a></li>
+                <li><a href="#Resume" className="nav-link scrollto"><span>Resume</span></a></li>
                 <li><a href="#portfolio" className="nav-link scrollto"><span>Portfolio</span></a></li>
                 <li><a href="#contact" className="nav-link scrollto"><span>Contact</span></a></li>
             </ul>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,72 @@
-// import React from 'react';
+import React from 'react';
 
-// function Resume() {
+function Resume() {
 
-//     return (
+    return (
         
-//     )
-// }
+        <section id="resume" className="resume second-bg">
+            <div className="container">
+    
+                <div>
+                    <h2>Resume</h2>
+                    <p>With my extensive training in a combination of technical and creative fields, I have worked in several mediums. </p>
+                    <p>Here are some of the tools I have been most recently focusing on.</p>
+                </div>
+    
+                <div className="row skills-content">
+        
+                    <div className="col-lg-6" data-aos="fade-up">
+        
+                    <div className="progress">
+                        <span className="skill">HTML <i className="percent">100%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+        
+                    <div className="progress">
+                        <span className="skill">CSS <i className="percent">95%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+        
+                    <div className="progress">
+                        <span className="skill">JavaScript <i className="percent">85%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+        
+                    </div>
+        
+                    <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+        
+                    <div className="progress">
+                        <span className="skill">Node.Js <i className="percent">80%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+        
+                    <div className="progress">
+                        <span className="skill">Illustrator <i className="percent">85%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+        
+                    <div className="progress">
+                        <span className="skill">Photoshop <i className="percent">75%</i></span>
+                        <div className="progress-bar-wrap">
+                        <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
 
-// export default Resume;
+export default Resume;
