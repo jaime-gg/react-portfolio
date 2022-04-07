d516abf8be0ed24a57297fe86d3cc0198a4ba016
jester <jaimebuddy98@gmail.com>
2022-04-07 13:03:01 -0500

    imported

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero'; 
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume'
+import Contact from './components/Contact';
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
+      <main>
+        <body>
+          <Hero /> 
+          <About />
+          <Portfolio />
+          <Resume />
+          <Contact />
+        </body>
+        <footer>
+          <Footer />
+        </footer>
+      </main>
     </div>
   );
 }
