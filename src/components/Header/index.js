de0e6ff8e4bc770d9447b9af01b25dce616a5c71
jester <jaimebuddy98@gmail.com>
2022-04-07 20:42:44 -0500

    nav menu reformatted and added active css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,21 +4,23 @@ import './style.css';
 function Header({ currentPage, handlePageChange }) {
 
     return (
-        <nav id="navbar" className="nav-menu navbar navbar-expand-lg">
-            <ul className="">
-                <li>
-                    <a href="#about" onClick={() => handlePageChange('About')} className="nav-link scrollto"><span>About</span></a>
-                </li>
-                <li>
-                    <a href="#resume" onClick={() => handlePageChange('Resume')} className="nav-link scrollto"><span>Resume</span></a>
-                </li>
-                <li>
-                    <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className="nav-link scrollto"><span>Portfolio</span></a>
-                </li>
-                <li>
-                    <a href="#contact" onClick={() => handlePageChange('Contact')} className="nav-link scrollto"><span>Contact</span></a>
-                </li>
-            </ul>
+        <nav>
+            <nav id="navbar" className="nav-menu navbar1">
+                <ul className="justify-content-center">
+                    <li>
+                        <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'active nav-link scrollto' : 'nav-link scrollto'} ><span>About Me</span></a>
+                    </li>
+                    <li>
+                        <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Resume</span></a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Portfolio</span></a>
+                    </li>
+                    <li>
+                        <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'active nav-link scrollto' : 'nav-link scrollto'}><span>Contact</span></a>
+                    </li>
+                </ul>
+            </nav>
         </nav>
     )
 }
